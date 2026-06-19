d7e47a3f575503a17d9dd825dc373ae535cce650
Zhachory Volker <me@zhachory.com>
2026-06-19 17:28:35 -0400

    fix: repair solver interface and GA compatibility

--- solve_interface.py
+++ solve_interface.py
@@ -1,83 +1,67 @@
 from collections import Counter
-import numpy as np
-import tasks_pb2
+import random
+
 
 class SolverInterface:
-    # Tasks are a list of tasks defined in the tasks.proto file.
-    # The schedule will be defined by a list of task ids, where each 
-    # element is a 30 minute increment. 
-    # num_of_increments = default 12 hours
+    # Tasks are a list of tasks defined in tasks.proto.
+    # The schedule is a list of task ids, one per time increment.
     def __init__(self, tasks=None, num_of_increments=24):
-        self.tasks = tasks
-        self.schedule = [-1]*num_of_increments
+        self.tasks = tasks or []
+        self.schedule = [-1] * num_of_increments
 
     def get_schedule(self):
         return self.schedule
 
     def set_tasks(self, tasks):
-        self.tasks = tasks
+        self.tasks = tasks or []
 
     def solve_schedule(self):
-        self.schedule = self.__solve_impl()
-    
-    # Please implement for your classes
-    def __solve_impl(self):
+        self.schedule = self._solve_impl()
+        return self.schedule
+
+    def _solve_impl(self):
         raise NotImplementedError
-    
-    # Update the task list with the tasks you have done with the schedule.
-    # If the schedule is not filled, it does nothing.
+
     def update_tasks(self):
-        schedule_counter = Counter(self.schedule)
-        task_counter = self.__get_counter_from_tasktime()
-        task_counter.subtract(schedule_counter)
+        schedule_counter = Counter(x for x in self.schedule if x != -1)
         for task in self.tasks:
-            if task.id in task_counter
-                task.time_required -= task_counter[task.id]
+            task.time_required = max(0, task.time_required - schedule_counter.get(task.id, 0))
 
+    def _get_counter_from_tasktime(self):
+        return Counter({task.id: task.time_required for task in self.tasks})
 
-    def __find_random_filled_unit(schedule):
-        # To include randomness, we will choose a random starting point; then we
-        # we will go through the array, wrapping to the beginning to find an empty
-        # unit. Will return -1 if there are no filled spots
-        start = int(np.floor(np.random.random() * (len(schedule) - 1)))
-        pos = start + 1
-        while pos != start:
-            if pos >= len(schedule):
-                pos = 0
+    def _add_time_unit(self, task_id, index):
+        task_counter = self._get_counter_from_tasktime()
+        if task_counter[task_id] > self.schedule.count(task_id) and self.schedule[index] == -1:
+            self.schedule[index] = task_id
+        return self.schedule
+
+    def _remove_time_unit(self, index):
+        self.schedule[index] = -1
+        return self.schedule
+
+    @staticmethod
+    def _find_random_filled_unit(schedule):
+        if not schedule:
+            return -1
+        start = random.randrange(len(schedule))
+        pos = start
+        while True:
             if schedule[pos] != -1:
                 return pos
-            pos += 1
-        return -1
-    
-    def __get_counter_from_tasktime(self):
-        # Converts the list of TASKS into a Counter object where the index is
-        # the index of the task
-        results = {}
-        for _, task in enumerate(self.tasks):
-            results[task.id] = task.time_required
-        return Counter(results)
+            pos = (pos + 1) % len(schedule)
+            if pos == start:
+                return -1
 
-
-    def __add_time_unit(self, task_id, index):
-        task_counter = self.get_counter_from_tasktime()
-        if task_counter[task_id] > 0 and schedule[index] == -1:
-            schedule[index] = task_id
-
-
-    def __remove_time_unit(schedule, tasks, index):
-        schedule[index] = -1
-        return schedule
-    
-    def __find_random_empty_unit(schedule):
-        # To include randomness, we will choose a random starting point; then we
-        # we will go through the array, wrapping to the beginning to find an empty
-        # unit. Will return -1 if we can't find an empty slot
-        start = int(np.floor(np.random.random() * (len(schedule) - 1)))
-        pos = start + 1
-        while pos != start:
-            if pos >= len(schedule):
-                pos = 0
+    @staticmethod
+    def _find_random_empty_unit(schedule):
+        if not schedule:
+            return -1
+        start = random.randrange(len(schedule))
+        pos = start
+        while True:
             if schedule[pos] == -1:
                 return pos
-            pos += 1
-        return -1
+            pos = (pos + 1) % len(schedule)
+            if pos == start:
+                return -1
